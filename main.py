85b8a7f087e1423d488acc32d34ab9dd58e81a60
Katadoka <katadoka@gmail.com>
2018-05-03 17:29:43 +0300

    update BD

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
-from flask import render_template
+from peewee import fn
 from modelsorm import User, Amount
 
 app = Flask(__name__)
@@ -19,25 +19,13 @@ def datanow():
 @app.route('/greeting/<name>')
 def show_name(name):
     User.get_or_create(username=name)
+    Amount.get_or_create(user=name, amount=0, date_time=datetime.now().isoformat())
     return 'Hello, {}'.format(name)
-
-
-def search_name(name):
-    query = (Amount
-        .select(Amount.user_id, Amount.amount)
-        .where(Amount.user_id == name))
-    return query
 
 
 @app.route('/names')
 def show_names():
-    query_names = (User
-                .select(User.username))
-
-    names = []
-    for row in query_names:
-        current_balance_name = 0
-        for amount in search_name(row.username):
-            current_balance_name += amount.amount
-        names.append('{} - {}'.format(row.username, current_balance_name))
-    return render_template('hello.html', names=names)
+    rows = list(Amount
+        .select(Amount.user, fn.SUM(Amount.amount).alias('balance'))
+        .group_by(Amount.user))
+    return render_template('hello.html', rows=rows)
